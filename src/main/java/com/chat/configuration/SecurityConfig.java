ccc6a00eb72eef89edf6e115a740f836cdd77d3b
Ewan Jee <830jiforcareer@gmail.com>
2025-01-14 15:38:33 +0900

    feat: security 설정

---
+++ src/main/java/com/chat/configuration/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.chat.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable()) // CORS는 별도 설정 사용
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/chat/**").permitAll()
+                        .pathMatchers("/ws/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}
