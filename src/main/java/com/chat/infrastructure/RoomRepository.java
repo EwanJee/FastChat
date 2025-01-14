15a9c10d5337a9500c10986dd2a63e604a30f46e
Ewan Jee <830jiforcareer@gmail.com>
2025-01-14 16:27:22 +0900

    refactor: softDelete 도입으로 인한 메서드 추가

--- src/main/java/com/chat/infrastructure/RoomRepository.java
+++ src/main/java/com/chat/infrastructure/RoomRepository.java
@@ -3,9 +3,14 @@ package com.chat.infrastructure;
 import com.chat.domain.chat.Room;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface RoomRepository extends ReactiveMongoRepository<Room, String> {
     Mono<Room> findByIdAndLastMessageAtNotNull(String id);
+
+    Mono<Room> findByIdAndDeletedAtIsNull(String id);
+
+    Flux<Room> findAllByDeletedAtIsNull();
 }
