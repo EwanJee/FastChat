e5c12ee4b814fa68700965aee17c21afd0f8012c
Ewan Jee <830jiforcareer@gmail.com>
2025-01-16 16:20:32 +0900

    feat: Participant Not Found 추가

--- src/main/java/com/chat/application/exception/ErrorCode.java
+++ src/main/java/com/chat/application/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     ROOM_NOT_FOUND("채팅방을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     INVALID_PASSWORD("비밀번호가 올바르지 않습니다.", HttpStatus.UNAUTHORIZED),
-    INVALID_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
+    INVALID_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
+    PARTICIPANT_NOT_FOUND("참여자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus status;
